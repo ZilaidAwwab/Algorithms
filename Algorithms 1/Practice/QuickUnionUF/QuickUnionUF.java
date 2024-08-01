4fed6c3d108539361a95d1c68083c1116dcad6e6
Zilaid Awwab <132026940+ZilaidAwwab@users.noreply.github.com>
2024-08-01 22:05:40 +0500

    Quick Union Algorithm

--- Algorithms 1/Practice/QuickUnionUF/QuickUnionUF.java
+++ Algorithms 1/Practice/QuickUnionUF/QuickUnionUF.java
@@ -1,29 +1,31 @@
 public class QuickUnionUF {
-    private int[] id;
+  private int[] id;
 
-    public QuickUnionUF(int N) {
-      id = new int[N];
-      // set id of each object to itself
-      for (int i = 0; i < N; i++) {
-        id[i] = i;
-      }
+  public QuickUnionUF(int N) {
+    id = new int[N];
+    // set id of each object to itself
+    for (int i = 0; i < N; i++) {
+      id[i] = i;
     }
+  }
 
-    public int root(int i) {
-      // choose parent pointer until reach root
-      while (i != id[i]) {
-        i = id[i];
-      }
-      return i;
+  private int root(int i) {
+    // choose parent pointer until reach root
+    while (i != id[i]) {
+      i = id[i];
     }
+    return i;
+  }
 
-    public boolean connected(int p, int q) {
-      return root(p) == root(q);
-    }
+  public boolean connected(int p, int q) {
+    // check if p and q have same roots
+    return root(p) == root(q);
+  }
 
-    public void union(int p, int q) {
-      int i = root(p);
-      int j = root(q);
-      id[i] = j;
-    }
+  public void union(int p, int q) {
+    // change root of p to point to root of q
+    int i = root(p);
+    int j = root(q);
+    id[i] = j;
+  }
 }
