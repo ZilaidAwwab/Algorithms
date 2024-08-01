ad911c1a1f8a79d931e31659fa673cd9bf4a6cb4
ZilaidAwwab <zilaidawwab786@gmail.com>
2024-08-01 22:16:59 +0500

    Quick Find Algorithm

---
+++ Algorithms 1/Practice/UnionFindUF/QuickFindUF.java
@@ -0,0 +1,27 @@
+public class QuickFindUF {
+    private int[] id;
+
+    public QuickFindUF(int N) {
+        id = new int[N];
+        // set id of each object to itself
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    public boolean connected(int p, int q) {
+        // checking whether p and q are in the same component
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+        // change all entries with id[p] to id[q]
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pid) {
+                id[i] = qid;
+            }
+        }
+    }
+}
