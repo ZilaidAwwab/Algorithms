6359a5bba3bd4fd2c5e6eac2fdea35dd349192e9
ZilaidAwwab <zilaidawwab786@gmail.com>
2024-09-05 22:00:36 +0500

    Symbol Table Client Code Implementation

---
+++ Algorithms 1/Practice/SymbolTables/SymbolTableClient.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.ST;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class SymbolTableClient {
+    public static void main(String[] args) {
+        ST<String, Integer> st;
+        st = new ST<String, Integer>();
+
+        for (int i = 0; !StdIn.isEmpty(); i++) {
+            String key = StdIn.readString();
+            st.put(key, i);
+        }
+
+        for (String s: st.keys()) {
+            StdOut.println(s + " " +st.get(s));
+        }
+    }
+}
